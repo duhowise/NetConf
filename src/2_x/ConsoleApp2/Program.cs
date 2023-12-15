41622103cba022995da8654a47ecf4456a0a6458
wise duho <wise.duho@hubtel.com>
2023-12-15 11:09:48 +0000

    Create separate Address class
    The Address class, which previously existed in Program.cs, has been extracted into a separate dedicated file - Address.cs.

--- src/2_x/ConsoleApp2/Program.cs
+++ src/2_x/ConsoleApp2/Program.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-
 namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Hello World!");
         }
     }
-}
-
-public class Address
-{
-    public string Street { get; set; }
-    public string City { get; set; }
-    public string ZipCode { get; set; }
 }
